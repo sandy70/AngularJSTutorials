d69d5f1e5fc497216172239a829af43eeb0f2567
sandy70 <swami.sandeep@gmail.com>
2016-12-18 20:26:10 -0500

    rest-api call & promises

--- PhoneBookApp/PhoneBookApp.controller.js
+++ PhoneBookApp/PhoneBookApp.controller.js
@@ -4,9 +4,12 @@
     contactApp.controller("PhoneBookAppCtrl", PhoneBookAppCtrl);
 
     function PhoneBookAppCtrl(contactDetailsSvc) {
-
+        var self = this;
         console.log("Iam PhoneBookAppCtrl !!!!");
-        this.contacts = contactDetailsSvc.contacts;
+        this.contacts = contactDetailsSvc.getContacts()
+                                         .then(function(data){
+                            self.contacts = data;
+        });
         this.selectContact = function (index) {
             this.selectedContact = this.contacts[index];
         }
